c1c63c363aba56016dbd08ad5b2d4a81a1feff1c
sidkha <ryan.byloos@student.umons.ac.be>
2020-07-08 14:01:06 +0200

    DatastreamPage added

---
+++ admin_frontend/src/components/DatastreamPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-form>
+      <b-row class="justify-content-md-center mb-5">
+        <label><h1>Datastream</h1></label>
+      </b-row>
+      <b-row class="justify-content-md-center mb-5">
+        <b-col>
+          <label><b>Features</b></label>
+          <b-form-radio-group :options="fragmentation_options" required></b-form-radio-group>
+        </b-col>
+        <b-col>
+          <label><b>Fragmentation strategies</b></label>
+        </b-col>
+      </b-row>
+    </b-form>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
