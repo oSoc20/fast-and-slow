583df3d0414b7a5a86e27c89fc2a46791a7f1b3c
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-07-10 12:02:29 +0200

    Working base project

--- admin-interface-frontend/src/App.vue
+++ admin-interface-frontend/src/App.vue
@@ -6,12 +6,11 @@
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
-  $vl-icon-font-location: "./assets";
-  @import "~@govflanders/vl-ui-core/src/scss/core";
+    $vl-icon-font-location: "assets/";
+    @import "~@govflanders/vl-ui-core/src/scss/core";
 </style>
