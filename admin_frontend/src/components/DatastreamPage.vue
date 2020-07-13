3559049dbd60c896f32801f25b95ea793eb0046b
stijnrosaer <stijn.rosaer@student.uantwerpen.be>
2020-07-13 10:46:20 +0200

    Parse shacl shape

--- admin_frontend/src/components/DatastreamPage.vue
+++ admin_frontend/src/components/DatastreamPage.vue
@@ -53,20 +53,92 @@
             this.fetchStream(this.$route.query.url)
         },
         methods: {
+            /**
+             * Combine nested parameters
+             * @param features
+             * @param nodes
+             * @param properties
+             * @returns {{parameter: {URI defining that parameter}}}
+             */
+            combine: function (features, nodes, properties) {
+
+                const parameter_list = {}
+                for (const key in features) {
+                    // Check if feature is not defining
+                    if (features[key].grouped === false) {
+                        let current = key;
+                        let url_list = [features[current].subject]
+                        let stop = false
+                        // See if features can be linked together
+                        while (!stop) {
+                            if (current !== key) {
+                                features[current].grouped = true
+                            }
+                            if (current in properties) {
+                                if (properties[current] in nodes) {
+                                    url_list.push(features[nodes[properties[current]]].subject)
+                                    current = nodes[properties[current]]
+                                    continue
+                                }
+                            }
+                            stop = true
+                        }
+                        // Determine show value of a feature
+                        let value = this.getShowValue(url_list)
+                        parameter_list[value] = url_list
+                    }
+
+
+                }
+                return parameter_list
+            },
+
+            /**
+             * Generate a display name for a list of property URI's
+             * @param url_list
+             * @returns {string}
+             */
+            getShowValue: function (url_list) {
+                let returnValue = ""
+                // Get parameter from every URI and combine them
+                url_list.reverse().forEach(url => {
+                    const split_token = (url.includes('#')) ? '#' : '/'
+                    const value = url.split(split_token).pop()
+                    returnValue = returnValue.concat(value + ' - ')
+                })
+                return returnValue.substring(0, returnValue.length - 3)
+            },
+
+            /**
+             * Fetch a specific data stream and add parameters to list
+             * @param url: Url that defines the data stream
+             * @returns {Promise<void>}
+             */
             fetchStream: async function (url) {
                 this.isBusy = true
-                console.log(url)
+                const features = {}
+                const nodes = {}
+                const properties = {}
                 try {
                     const {quads} = await rdfDereferencer.dereference(url)
                     quads.on('data', (quad) => {
-                        if (quad.predicate.value === 'https://www.w3.org/ns/shacl#path' && quad.object.value.includes('sosa')) {
-                            console.log(quad)
-                            const splitted = quad.object.value.split('/')
-                            this.feature_options.push({text: splitted[splitted.length - 1], value: quad.object.value})
+                        if (quad.predicate.value === 'https://www.w3.org/ns/shacl#node') {
+                            nodes[quad.object.value] = quad.subject.value
+                        } else if (quad.predicate.value === 'https://www.w3.org/ns/shacl#property') {
+                            properties[quad.object.value] = quad.subject.value
+                        } else if (quad.predicate.value === 'https://www.w3.org/ns/shacl#path') {
+                            features[quad.subject.value] = {'subject': quad.object.value, 'grouped': false}
                         }
-                        //remove spinner
-                        this.isBusy = false;
                     })
+                        .on('end', () => {
+                            //remove spinner
+                            this.isBusy = false;
+
+                            const feature_values = this.combine(features, nodes, properties)
+                            for (const feature in feature_values) {
+                                this.feature_options.push({text: feature, value: feature_values[feature]})
+                            }
+                        })
                 } catch (e) {
                     console.error(e)
                 }
