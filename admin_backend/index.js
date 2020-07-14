e483cc09b1d58fcb72f3cb22dea1aeab741e45e1
sidkha <ryan.byloos@student.umons.ac.be>
2020-07-14 12:17:09 +0200

    more debugging

--- admin_backend/index.js
+++ admin_backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const redis = require('redis');
+const redis = require('async-redis');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -18,45 +18,49 @@ app.get('/', (req, res) => {
     res.send('Something')
 })
 
-app.get('/streams', async (req, res) => {
-    // let streams = {}
-    let streams = await client.keys('*', async (err, keys) => {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-        for (let i = 0; i < keys.length; i++) {
-            let key = keys[i]
-            let header = await client.get(key, (err, result) => {
-                if (err) {
-                    console.log(err);
-                    throw err;
-                }
+app.get('/streams', (req, res) => {
+    let streams = {}
+    client.keys('*').then(async (keys) => {
+        // for await (let i = 0; i < keys.length; i++) {
+        let promises = []
+        for (let key of keys) {
+            // const key = keys[i]
+            console.log(key)
+            let promise = client.get(key).then((result) => {
                 let name_list = JSON.parse(result)
                 streams[key] = name_list[name_list.length - 1]
+            }).catch((err) => {
+                console.error(err)
             })
+            promises.push(promise)
         }
+        await Promise.all(promises)
+        console.log(streams)
+        res.json(streams)
+    }).catch((err) => {
+        console.error(err)
     })
-    // let client_keys = await client.keys('*', (err, keys) => {
-    //     if (err) {
-    //         console.log(err);
-    //         throw err;
-    //     }
-    //     for (let i = 0; i < keys.length; i++) {
-    //         const key = keys[i]
-    //         client.get(key, function (err, result) {
-    //             if (err) {
-    //                 console.log(err);
-    //                 throw err;
-    //             }
-    //             var name_list = JSON.parse(result)
-    //             streams[key] = name_list[name_list.length - 1]
-    //         })
-    //     }
-    // })
-    console.log(streams)
-    res.json(streams)
 })
+
+
+// app.get('/streams', (req, res) => {
+//     let streams = {}
+//     client.keys('*', (err, keys) => {
+//         if (err) return console.log(err)
+//         if (keys) {
+//             async.map(keys, (key, cb) => {
+//                 client.get(key, (error, value) => {
+//                     if (error) return cb(error)
+//                     let name_list = JSON.parse(value)
+//                     streams[key] = name_list[name_list.length - 1]
+//                 })
+//             })
+//         }
+//     })
+//     console.log(streams)
+//     res.json(streams)
+// })
+
 
 app.post('/stream', function (req, res) {
     console.log(req.body)
