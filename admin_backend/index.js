726e0ceae96f77954b7e5137054f04214d24ef3e
sidkha <ryan.byloos@student.umons.ac.be>
2020-07-15 16:37:05 +0200

    using query for fragmentation too

--- admin_backend/index.js
+++ admin_backend/index.js
@@ -79,12 +79,12 @@ app.post('/streams', async function (req, res) {
         })
 })
 
-app.get('/streams/properties/:url', (req, res) => {
+app.get('/streams/properties', (req, res) => {
     /**
      * Get all the streams with their latest name
      * @type {{}}
      */
-    const url = req.params.url
+    const url = req.query.url
 
     Stream.findOne({url: url})
         .then(result => {
