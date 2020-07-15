1000dbfb3ede41568b86d5b8a3aa8923ad7d78aa
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-07-15 16:13:50 +0200

    Fix info tile text not on same height + modified key in v-for

--- admin-interface-frontend/src/pages/Home.vue
+++ admin-interface-frontend/src/pages/Home.vue
@@ -5,34 +5,26 @@
                 <vl-title tag-name="h1">Fast and Slow</vl-title>
             </vl-column>
 
-
             <vl-column>
                 <vl-title tag-name="h2">Event streams in progress</vl-title>
             </vl-column>
-            <vl-column width="4" v-for="line of inProgress" :key="line">
+            <!-- TODO: a better key can be used here -->
+            <vl-column width="4" v-for="line of inProgress" :key="line.name + line.fragmentation">
                 <vl-info-tile
-                    v-vl-equal-height
-                    tag-name="div"
-                    :title="line.name"
-                    :subtitle="line.description"
-                    class="custom-tile"> 
+                        v-vl-equal-height
+                        tag-name="div"
+                        :title="line.name"
+                        :subtitle="line.description">
 
-                    <span class="info-tile-span-custom">
+                    <div class="container-scoped">
                         <strong>Is loaded: </strong>
-                        <vl-icon v-if="line.loaded" icon="check-circle" mod-large />
-                        <vl-icon v-else icon="alert-circle" mod-large />
+                        <vl-icon v-if="line.loaded" icon="check-circle" mod-large/>
+                        <vl-icon v-else icon="alert-circle" mod-large/>
                         <br>
                         <strong>Fragmentation: </strong> {{line.fragmentation}}
-                    </span>
+                    </div>
                 </vl-info-tile>
             </vl-column>
-            
-
-
-
-
-
-
 
 
             <vl-column width="10">
@@ -41,7 +33,9 @@
                 </vl-title>
             </vl-column>
             <vl-column width="2">
-                <vl-button v-vl-positioning:float-right v-vl-modal-toggle="'stream-modal'" icon="add" mod-icon-before mod-narrow>Add new</vl-button>
+                <vl-button v-vl-positioning:float-right v-vl-modal-toggle="'stream-modal'" icon="add" mod-icon-before
+                           mod-narrow>Add new
+                </vl-button>
             </vl-column>
             <vl-column>
                 <vl-data-table mod-line>
@@ -55,18 +49,18 @@
                     </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="stream of streams" :key="stream.name">
-                            <td>{{stream.name}}</td>
-                            <td>{{stream.online}}</td>
-                            <td>{{stream.progress}}</td>
-                            <td>
-                                <vl-icon v-if="stream.loaded" icon="check-circle" mod-large />
-                                <vl-icon v-else icon="alert-circle" mod-large />
-                            </td>
-                            <td>
-                                <vl-button to="/stream" mod-narrow> View details</vl-button>
-                            </td>
-                        </tr>
+                    <tr v-for="stream of streams" :key="stream.name">
+                        <td>{{stream.name}}</td>
+                        <td>{{stream.online}}</td>
+                        <td>{{stream.progress}}</td>
+                        <td>
+                            <vl-icon v-if="stream.loaded" icon="check-circle" mod-large/>
+                            <vl-icon v-else icon="alert-circle" mod-large/>
+                        </td>
+                        <td>
+                            <vl-button to="/stream" mod-narrow> View details</vl-button>
+                        </td>
+                    </tr>
                     </tbody>
                 </vl-data-table>
             </vl-column>
@@ -75,19 +69,18 @@
             </vl-column>
 
 
-
-
         </vl-grid>
     </vl-layout>
 </template>
 
 <script>
-import DatastreamModal from "../components/DatastreamModal";
+    import DatastreamModal from "../components/DatastreamModal";
+
     export default {
-    name: "Home",
-    components: {DatastreamModal},
-    data () {
-        var streams = [
+        name: "Home",
+        components: {DatastreamModal},
+        data() {
+            var streams = [
                 {
                     name: "Address",
                     online: 5,
@@ -103,55 +96,49 @@ import DatastreamModal from "../components/DatastreamModal";
                     url: 'http://streams.datapiloten.be/observations'
                 }
             ];
-        var inProgress = [
+            var inProgress = [
                 {
                     name: "Address",
-                    description:"This event stream contains all the base registers of Flanders",
+                    description: "This event stream contains all the base registers of Flanders",
                     loaded: true,
                     fragmentation: "Time",
 
                 },
                 {
                     name: "SensorObservations",
-                    description:"This event streams contains air quality data",
+                    description: "This event streams contains air quality data",
                     loaded: false,
                     fragmentation: "Geospatial",
 
                 },
                 {
                     name: "Address",
-                    description:"This event stream contains all the base registers of Flanders",
+                    description: "This event stream contains all the base registers of Flanders",
                     loaded: true,
                     fragmentation: "Text",
                 }
             ];
-        inProgress.forEach(item => {
-            if(item.loaded){
-                item.template = "<strong>Description: </strong>" + item.description + "<br>" + "<strong>Is loaded: </strong>" + '<vl-icon icon="check-circle" mod-large> </vl-icon>' + '<br>' + "<strong>Fragmentation: </strong>" + item.fragmentation;
-            }
-            else{
-                item.template = "<strong>Description: </strong>" + item.description + "<br>" + "<strong>Is loaded: </strong>" + '<vl-icon icon="alert-circle" mod-large </vl-icon>' + "<br>" + "<strong>Fragmentation: </strong>" + item.fragmentation;
-            }
-            
-        });
+            inProgress.forEach(item => {
+                if (item.loaded) {
+                    item.template = "<strong>Description: </strong>" + item.description + "<br>" + "<strong>Is loaded: </strong>" + '<vl-icon icon="check-circle" mod-large> </vl-icon>' + '<br>' + "<strong>Fragmentation: </strong>" + item.fragmentation;
+                } else {
+                    item.template = "<strong>Description: </strong>" + item.description + "<br>" + "<strong>Is loaded: </strong>" + '<vl-icon icon="alert-circle" mod-large </vl-icon>' + "<br>" + "<strong>Fragmentation: </strong>" + item.fragmentation;
+                }
 
-        return {
-            streams,
-            inProgress
+            });
+
+            return {
+                streams,
+                inProgress
+            }
         }
     }
-}
 </script>
 
 <style lang="scss">
-
-    vl-info-tile.custom-tile{
-        position: relative;
-    }
-
-    span.info-tile-span-custom{
+    .container-scoped{
         position: absolute;
-        bottom: 0;
-        right: 0;
+        bottom: 0px ;
+        margin-bottom: 1em;
     }
 </style>
